7fa71d29dcdb6dd57f5dfeae2dc5394f4b8a4e0e
anastasiak36 <akelnik.9@gmail.com>
2022-01-14 10:48:35 -0500

    fixed js errors

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+ctx.arc(100, 25, 20, 0, Math.PI * 2);
 ctx.fillStyle = "#8BD9C2";
 ctx.fill();
 ctx.closePath();
