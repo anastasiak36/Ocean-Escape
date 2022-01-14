e0ad847a971ce1615bfb6dd9d720e4464d0d5277
anastasiak36 <akelnik.9@gmail.com>
2022-01-14 10:44:40 -0500

    fixed background image and pos

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
-alert("hi");
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+ctx.fillStyle = "#8BD9C2";
+ctx.fill();
+ctx.closePath();
