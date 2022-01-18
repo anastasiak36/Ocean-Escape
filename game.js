245bf3befe07420b6df92fd63fd1dbfab701fb53
riasha24 <riajshah@gmail.com>
2022-01-18 08:49:53 -0500

    Tried to get fish on screen

--- game.js
+++ game.js
@@ -2,7 +2,13 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 function draw() {
-    var fish = getElementById('fish');
-    ctx.drawImage(fish, 10, ctx.height - 10);
+    //var fish = document.getElementById('fish');
+    var img = new Image();
+    img.addEventListener('load', function() {
+        ctx.drawImage(img, 10, ctx.height - 100);
+    }, false);
+    img.src = "../images/fish_mouth_closed.png";
+    //ctx.drawImage(img, 10, ctx.height - 100);
+    
 }
 draw();
