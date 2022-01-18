564857eeef5c79c63e3d5b239c7f86df7bd92707
anastasiak36 <akelnik.9@gmail.com>
2022-01-18 08:19:40 -0500

    added fish images

--- game.js
+++ game.js
@@ -1,8 +1,3 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(100, 25, 20, 0, Math.PI * 2);
-ctx.fillStyle = "#8BD9C2";
-ctx.fill();
-ctx.closePath();
