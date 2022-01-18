b41388b0a04491869afaf4ed978faed6394ef5a6
riasha24 <riajshah@gmail.com>
2022-01-18 08:25:47 -0500

    Added draw function in JavaScript

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+function draw() {
+    var fish = getElementById('fish');
+    ctx.drawImage(fish, 10, ctd.height - 10);
+}
+draw();
