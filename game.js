f6195177274ee182ebca214d5b3bc08c97f4a866
anastasiak36 <akelnik.9@gmail.com>
2022-01-18 08:33:33 -0500

    fixed background andn set fish dimensions

--- game.js
+++ game.js
@@ -3,6 +3,6 @@ var ctx = canvas.getContext("2d");
 
 function draw() {
     var fish = getElementById('fish');
-    ctx.drawImage(fish, 10, ctd.height - 10);
+    ctx.drawImage(fish, 10, ctx.height - 10);
 }
 draw();
