b4cd5c7def0e1f9824bd939ddaff49cdb8012ba3
anastasiak36 <akelnik.9@gmail.com>
2022-01-20 14:48:00 -0500

    got the fish to move up

--- game.js
+++ game.js
@@ -1,63 +1,52 @@
-// var canvas = document.getElementById("myCanvas");
-// var ctx = canvas.getContext("2d");
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var x = 10;
+var y = 450;
+var img = document.getElementById("fish");
+var mouth = true;
+var spacePressed = false;
 
-// function draw() {
-//     //var fish = document.getElementById('fish');
-//     var img = new Image();
-//     img.addEventListener('load', function() {
-//         ctx.drawImage(img, 10, ctx.height - 100);
-//     }, false);
-//     img.src = "../images/fish_mouth_closed.png";
-//     //ctx.drawImage(img, 10, ctx.height - 100);
-    
-// }
-// draw();
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key == "ArrowRight" || e.key == "Right") {
+        spacePressed = true;
+    }
+} 
+
+function keyUpHandler(e) {
+    if(e.key == "ArrowRight" || e.key == "Right" ) {
+        spacePressed = false;
+    }
+}
+function drawPlayer() {
+    ctx.drawImage(img, x, y, 100, 50);
+    img.height = "100px";
+    img.width = "150px";
+}
 window.onload = function() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    var x = 10;
-    var y = 450;
-    var img = document.getElementById("fish");
-    var mouth = true;
-    var spacePressed = false;
+    drawPlayer();
+}
+function move() {
+    y -= 50;
+}
+setInterval(move, 10000);
 
-    document.addEventListener("keydown", keyDownHandler, false);
-    document.addEventListener("keyup", keyUpHandler, false);
-
-    function keyDownHandler(e) {
-        if (e.key == "Right" || e.key == "ArrowRight") {
-            spacePressed = true;
+function draw() {
+    //sort of works but is glitchy
+    if (spacePressed) {
+        if (mouth) {
+            img.src = "images/fish_mouth_closed.png";
+            mouth = false;
         }
-    } 
-
-    function keyUpHandler(e) {
-        if(e.key == "Right" || e.key == "ArrowRight") {
-            spacePressed = false;
+        else {
+            img.src = "images/fish_mouth_open.png";
+            mouth = true;
         }
     }
-    function drawPlayer() {
-        ctx.drawImage(img, x, y, 100, 50);
-        img.height = "100px";
-        img.width = "150px";
-    }
-    function draw() {
-       drawPlayer();
-       if (spacePressed) {
-           if (mouth) {
-               img.src = "images/fish_mouth_closed.png";
-               mouth = false;
-           }
-           else {
-               img.src = "images/fish_mouth_open.png";
-               mouth = true;
-           }
-       }
-    }
-    function move() {
-        y -= 25;
-    }
-    
-    draw();
-    
-    
-  };
+    ctx.clearRect(0, 0, c.width, c.height);
+    drawPlayer();
+    requestAnimationFrame(draw);
+ }
+ draw();
